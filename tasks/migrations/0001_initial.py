a6e894d392580573aa0ed76ccd6b6a7ad4ef3fab
g.poulos <g.poulos@migration.gov.gr>
2026-06-05 15:58:37 +0300

    Format code with black

--- tasks/migrations/0001_initial.py
+++ tasks/migrations/0001_initial.py
@@ -7,18 +7,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Task',
+            name="Task",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('description', models.TextField(blank=True)),
-                ('is_done', models.BooleanField(default=False)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=200)),
+                ("description", models.TextField(blank=True)),
+                ("is_done", models.BooleanField(default=False)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
             ],
         ),
     ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
-
 from django.http import JsonResponse
 
 from .models import Task
 
 
 def task_list(request):
-    data = list(Task.objects.values("id", "title", "description", "is_done", "created_at"))
+    data = list(
+        Task.objects.values("id", "title", "description", "is_done", "created_at")
+    )
     return JsonResponse(data, safe=False)
