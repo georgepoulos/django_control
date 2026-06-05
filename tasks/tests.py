b6ae45ec5f7ac5bdee29be75bc80f1b99be108cb
g.poulos <g.poulos@migration.gov.gr>
2026-06-05 15:07:30 +0300

    install app

---
+++ tasks/tests.py
@@ -0,0 +1,14 @@
+
+from django.test import TestCase
+from .models import Task
+
+
+class TaskModelTest(TestCase):
+    def test_task_creation(self):
+        task = Task.objects.create(
+            title="Learn CI/CD",
+            description="Practice Django pipeline",
+            is_done=False,
+        )
+        self.assertEqual(task.title, "Learn CI/CD")
+        self.assertFalse(task.is_done)
---
+++ tasks/views.py
@@ -0,0 +1,8 @@
+
+from django.http import JsonResponse
+from .models import Task
+
+
+def task_list(request):
+    data = list(Task.objects.values("id", "title", "description", "is_done", "created_at"))
+    return JsonResponse(data, safe=False)
