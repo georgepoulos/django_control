b22ac8a47bb5fa67f2d436333b848407409588e4
g.poulos <g.poulos@migration.gov.gr>
2026-06-06 15:55:39 +0300

    home page ruff fix

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, health, task_list
+from .views import health, home, task_list
 
 urlpatterns = [
     path("tasks/", task_list, name="task_list"),
